c72c621e09b84ab01cdfc910ef820abceab2583c
Angela Francisco <angelafrancisco@Nathans-MacBook-Pro-2.local>
2022-02-25 12:54:36 -0800

    Added animation for hamburger to x and updated jQuery click function

--- app.js
+++ app.js
@@ -2,19 +2,16 @@ console.log(`I AM LINKED`);
 
 // JQUERY SCRIPT //
 $(() => {
-    //  action: user clicks menu icon (hamburger)
+    //  on mobile, user clicks hamburger
     $('.nav-hamburger-container').click(() => {
-        //  add new class to nav
-        $('.nav-link-list').addClass('menu-open');
-        console.log(`MENU WAS CLICKED`);
-        // hide or swap? hamburger for X
+        // animation on hamburger divs into X
+        $('.nav-hamburger').toggleClass('move');
+        // add new class to hamburger, or should this only be on menu?
+        // $('.nav-hamburger-container').toggleClass('open');
+        //  nav-link-list gets toggleClass
+        $('.nav-link-list').toggleClass('open');
+        console.log(`MENU IS CLICKED`);
 
-    });
-    //  action: user clicks the X to close menu
-    $('.menu-close').click(() => {
-        //  remove 'menu-open' class
-        $('.nav-link-list').removeClass('menu-open');
-        console.log(`MENU HAS CLOSED`);
     });
 
 });
