abab726a3daa5736859045683cd49f40b620efc0
Angela Francisco <angelafrancisco@Nathans-MacBook-Pro-2.local>
2022-02-25 15:22:14 -0800

    Added resume pg and jq attempt to expand mobile menu down

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ console.log(`I AM LINKED`);
 $(() => {
     //  on mobile, user clicks hamburger
     $('.nav-hamburger-container').click(() => {
+        console.log(`MENU IS CLICKED`);
         // animation on hamburger divs into X
         $('.nav-hamburger').toggleClass('move');
         // add new class to hamburger, or should this only be on menu?
         // $('.nav-hamburger-container').toggleClass('open');
-        //  nav-link-list gets toggleClass
-        $('.nav-link-list').toggleClass('open');
-        console.log(`MENU IS CLICKED`);
-
+        // create new div, 3rd child to .nav-header
+        $('.nav-header').append('<div class="menu-block">');
+        //  add toggleClass to nav-link-list 
+        const $menuOpen = $('.nav-link-list').toggleClass('open');
+        // new open class moved to new div
+        $menuOpen.appendTo('.menu-block');
     });
 
 });
