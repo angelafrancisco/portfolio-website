e425d8c1bf7accfc09ed105dda9ad82e0e41097c
Angela Francisco <angelafrancisco@Nathans-MacBook-Pro-2.local>
2022-02-25 15:54:09 -0800

    Fixed jquery and css for mobile menu append/remove div

--- app.js
+++ app.js
@@ -7,14 +7,23 @@ $(() => {
         console.log(`MENU IS CLICKED`);
         // animation on hamburger divs into X
         $('.nav-hamburger').toggleClass('move');
-        // add new class to hamburger, or should this only be on menu?
-        // $('.nav-hamburger-container').toggleClass('open');
-        // create new div, 3rd child to .nav-header
-        $('.nav-header').append('<div class="menu-block">');
-        //  add toggleClass to nav-link-list 
-        const $menuOpen = $('.nav-link-list').toggleClass('open');
-        // new open class moved to new div
-        $menuOpen.appendTo('.menu-block');
+        // check element for .open class
+        const $menuOpen = $('.nav-link-list').hasClass('open');
+        if ($menuOpen) {
+            // if true, remove class .open
+            $('.nav-link-list').removeClass('open');
+            // move to right container
+            $('.nav-link-list').appendTo('.nav-container.right');
+            // remove div element
+            $('.nav-header').remove('div.menu-block');
+        } else {
+            // if false, add class .open
+            $('.nav-link-list').addClass('open');
+            // create new div, 3rd child to .nav-header
+            $('.nav-header').append('<div class="menu-block">');
+            // new open class moved to new div
+            $('.nav-link-list').appendTo('.menu-block');
+        }
     });
 
 });
